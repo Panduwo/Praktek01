03b857cc5f1e838688e4be6e9f3a820624215bb9
panduwo <hudhapandu@gmail.com>
2023-05-28 04:21:04 +0800

    Mengisi Nilai Atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -13,5 +13,9 @@ public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Pandu = new Mahasiswa();
         
+        Pandu.Nama = "Pandu Hudha Prabowo";
+        Pandu.NPM = "2110010232";
+        Pandu.IPK = 4.0;
+        
     }
 }
